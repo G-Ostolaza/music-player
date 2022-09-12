bbf747ce08d58c04103a00eb17287451f00a7826
Gabriel Ostolaza <Gabe.Osto@gmail.com>
2022-09-12 15:45:04 -0700

    initialized song variable,songIndex, and created loadSong function and playSong function

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
+
+// Song titles
+const songs = ['Bach', 'Mozart', 'Beethoven'];
+
+// Keep track of song
+let songIndex = 2;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
